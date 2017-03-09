0a29b215c8391e951385a732aa73dbc2d983b16b
RAJVEER <RAJVEER@RAJ>
2017-03-09 11:36:21 +0530

    first commit

---
+++ Assignment/mysrc/constructor/chaining/A.java
@@ -0,0 +1,18 @@
+package constructor.chaining;
+
+public class A {
+   A(int j){
+	   
+   }
+   A(){
+	   
+   }
+}
+class B extends A{
+
+	/*B(int j) {
+		super(j);
+		// TODO Auto-generated constructor stub
+	}
+	*/
+}
